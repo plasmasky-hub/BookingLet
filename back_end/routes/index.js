19d54ee5d0a4f6227118a0524cd3bac7664d8ab3
Jet Zhang <lovetrianglezym@hotmail.com>
2022-06-05 15:25:49 +1000

    change the router

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const userRouter = require('./user');
 const orderRouter= require('./orders');
-const serviceInfoRouter = require('./serviceInfo');
+// const serviceInfoRouter = require('./serviceInfo');
 
 const mainRouter = express.Router();
-const router = express.Router();
+
 
 mainRouter.use('/user', userRouter);
-router.use('/orders', orderRouter);
-router.use('/serviceInfo', serviceInfoRouter)
+mainRouter.use('/orders', orderRouter);
+// router.use('/serviceInfo', serviceInfoRouter)
 
 
-module.exports = {mainRouter,router};
+module.exports = {mainRouter};
