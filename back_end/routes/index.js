4df9a97af2967b35a9d4bc09c49bd555955fe213
Jet Zhang <lovetrianglezym@hotmail.com>
2022-06-05 16:59:24 +1000

    Simple CRUD of Order

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const userRouter = require('./user');
 const orderRouter= require('./orders');
-// const serviceInfoRouter = require('./serviceInfo');
+
 
 const mainRouter = express.Router();
 
 
 mainRouter.use('/user', userRouter);
 mainRouter.use('/orders', orderRouter);
-// router.use('/serviceInfo', serviceInfoRouter)
+
 
 
 module.exports = {mainRouter};
