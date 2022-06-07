170b7d6023c9bd8989b44a289ed879216ccff834
plasmasky-hub <61955808+plasmasky-hub@users.noreply.github.com>
2022-06-07 14:44:36 +0930

    add user CRUD

--- back_end/controllers/user.js
+++ back_end/controllers/user.js
@@ -2,24 +2,110 @@ const User = require('../modules/user');
 
 async function getAllUsers(req, res){
     console.log('Finding all users...');
-    const users = await User.find();
 
-    res.json(users);
+    try {
+        const users = await User.find();
+
+        res.json(users);
+    }
+    catch{
+        console.log('Error in Finding all users!');
+    }
 }
 
 async function addUser(req, res){
     console.log('Adding a new user...');
-    const { name, tel, email } = req.body;
+    try{
+        const { 
+            name, 
+            tel, 
+            email
+        } = req.body;
 
-    const newUser = new User({ name, tel, email });
-    await newUser.save();
+        const newUser = new User({ 
+            name, 
+            tel, 
+            email 
+        });
+        await newUser.save();
 
-    res.status(201).json(newUser);
+        res.status(201).json(newUser);
+    }
+    catch (error) {
+        console.log('Error in adding user!');
+        res.status(422).json('Error in adding user!');
+    }
 }
 
-async function updateUserByID(){};
-async function getUserByID(){};
-async function deleteUserByID(){};
+async function updateUserByID(req, res){
+    console.log('Updating user\'s info...');
+    try{
+        const {id} = req.params;
+        const { 
+            name, 
+            tel,
+            email
+        } = req.body;
+
+        // data validation
+
+        const user = await User.findByIdAndUpdate( id, {
+            name,
+            tel,
+            emal
+        });
+
+        if( !user ){
+            return res.status(404).json('User not found!');
+        }
+        res.json(user);
+    }
+    catch{
+        res.json('Error in Updating user!');
+    }
+
+};
+
+async function getUserByID(req, res){
+
+    console.log('Getting user\'s info...');
+    try{
+        const {id} = req.params;
+        const user = await User.findById(id);
+
+        if( !user ){
+            return res.status(404).json({
+                error: 'User info not found!',
+            });
+        }
+
+        res.json(user);
+    }
+    catch{
+        res.json('Error in finding user!');
+    }
+
+};
+
+async function deleteUserByID(req, res){
+    console.log('Deleting user\'s info...');
+    try{
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+
+        if( !user ){
+            return res.status(404).json({
+                error: 'User info not found!',
+            });
+        }
+
+        res.json(user);
+    }
+    catch{
+        res.json('Error in deleting user!');
+    };
+    
+};
 
 module.exports = {
     getAllUsers,
