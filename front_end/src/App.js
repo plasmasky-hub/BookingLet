6ce2b79f4c8867aba9981b4e300192665b7e5894
Anita-XR-LIU <scorpioliu1108@gmail.com>
2022-06-09 17:29:19 +0930

    Adding & styling Header

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import HeaderWrapper from './components/Header';
 
-function App() {
+const SearchBar = () => {
   return (
-    <div>bookinglet</div>
-  );
+    <div>SearchBar</div>
+  )
+}
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+const RegisterBtn = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <HeaderWrapper />
+      <SearchBar />
+      <Login />
+      <RegisterBtn />
+    </div>
+    
+  )
 }
 
 export default App;
--- front_end/src/components/Header/Login_Header.js
+++ front_end/src/components/Header/Login_Header.js
@@ -1,60 +1,26 @@
-import * as React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import Button from '@mui/material/Button';
-import styled from '@emotion/styled/types/base';
-import AppBar from '@mui/material/AppBar';
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
 
-
-const HeaderWrapper = styled.div`
-    width: 100%;
-    height: auto;
-    background: #fff;
-    border-shadow: 0 3px 6px #000;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-`
-
-const Logo = styled.h1`
-    font-size: 1.5rem;
-    font-weight: 700;
-    font-family: Helvetica;
-    color: #000;
-    min-width: 50px;
-    height: auto;
-`;
-
-const SearchBar = styled.div`
-
-`
-
-const SearchBarIcon =
-
-const UserBanner =
-
-const Login =
-
-const LoginIcon =
-    
-const RegisterBtn =
-
-return (
-    <HeaderWrapper>
-        <Logo>
+const HeaderWrapper = () => {
+    return (
+        <div class="Header_Wrapper">
+        <div class="Logo">
             BookingLet
-        </Logo>
-        <SearchBar>
-            <SearchBarIcon></SearchBarIcon>
-        </SearchBar>
-        <UserBanner>
-            <Login>
+        </div>
+        <div class="SearchBar">
+            <div class="SearchIcon"></div>
+        </div>
+        <div class="UserBanner_Wrapper">
+            <div class="User_Login">
+                <div class="LoginIcon"></div>
                 Login
-                <LoginIcon></LoginIcon>
-            </Login>
-            <RegisterBtn>
+            </div>
+            <div class="UserRegister">
                 Register
-            </RegisterBtn>
-        </UserBanner>
-    </HeaderWrapper>
-);
+            </div>
+        </div>
+    </div>
+    )
+  }
 
+  export default HeaderWrapper;
