9476e074b6d055a73472f24580e59f23468d9311
Jet Zhang <lovetrianglezym@hotmail.com>
2022-06-09 17:50:56 +1000

    add two routers

--- back_end/routes/orders.js
+++ back_end/routes/orders.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllOrders, addOrder, updateOrderByID, getOrderByID, deleteOrderByID } = require('../controllers/order');
+const { getAllOrders, addOrder, updateOrderByID, getOrderByID, deleteOrderByID,addOrderToUser, removeOrderFromUser } = require('../controllers/order');
 
 const orderRouter = express.Router();
 
@@ -8,5 +8,7 @@ orderRouter.post('', addOrder);
 orderRouter.get('/:id', getOrderByID);
 orderRouter.put('/:id', updateOrderByID);
 orderRouter.delete('/:id', deleteOrderByID);
+orderRouter.post('/:orderId/user/:userId', addOrderToUser);
+orderRouter.delete('/:orderId/user/:userId', removeOrderFromUser);
 
 module.exports = orderRouter;
