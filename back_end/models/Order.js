3bba77aac6458cf330fe0a86ade47bd8a7745111
Jet Zhang <lovetrianglezym@hotmail.com>
2022-06-12 17:05:46 +1000

    change the type of relevant ID

--- back_end/models/Order.js
+++ back_end/models/Order.js
@@ -18,15 +18,15 @@ const Schema = new mongoose.Schema({
         default:false
     },
     userId:{
-        type : String,
+        type : mongoose.Types.ObjectId,
         required:true  
     },
     storeId:{
-        type : String,
+        type : mongoose.Types.ObjectId,
         required:true  
     },
     serviceInfoId:{
-        type : String,
+        type : mongoose.Types.ObjectId,
         required:true  
     },
     optionInfo:{
