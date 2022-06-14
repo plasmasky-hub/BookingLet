d7bcdb6c98d71773853c6f9d7742f2d4db16bf7a
Harry <harry515952831@gmail.com>
2022-06-14 20:03:37 +1000

    dev

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,12 +2,16 @@ import Banner from './components/HomePage-banner/index';
 import Highlights from './components/filter/index';
 import Register from './components/Register';
 import Footer from './components/Footer';
+import StoreDisplay from './components/HomePage_StoreDisplay/StoreDisplay';
+import Header from './pages/Login_Header/Login_Header';
 
 function App() {
   return (
     <div>
       <div>
+        <Header />
         <Banner />
+        <StoreDisplay />
         <Register />
         <Footer />
       </div>
