0f93e7234cfc2a1c10b68498c8ad8a471c0e4fe6
yvonne1199 <62100248+yvonne1199@users.noreply.github.com>
2022-06-16 01:18:23 +0930

    search

--- back_end/routes/store.route.js
+++ back_end/routes/store.route.js
@@ -5,12 +5,14 @@ const {
     addStore,
     updateStoreById,
     discardStoreById,
+    search,
 } = require('../controllers/store.controller');
 
 const storeRouter = express.Router();
 
 storeRouter.get('', getAllStores);
 storeRouter.post('', addStore);
+storeRouter.post('/search', search);
 storeRouter.get('/:id', getStoreById);
 storeRouter.put('/:id', updateStoreById);
 storeRouter.delete('/:id', discardStoreById);
