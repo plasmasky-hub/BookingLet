9eb2f36b7443b2922725e8793c9191c25bedc7c9
Anita-XR-LIU <scorpioliu1108@gmail.com>
2022-06-17 22:41:25 +0930

    Added {theme}

---
+++ front_end/src/components/UI/theme.js
@@ -0,0 +1,77 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const createMuiTheme = {
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#D76D6D',
+            dark: '#D76D6D',
+            light: '#E27777',
+        },
+        secondary: {
+            main: '#7B8B6F',
+        },
+        background: {
+            default: '#FBFBFB',
+            paper: '#D9D9D9',
+        },
+        text: {
+            primary: '#000000',
+            secondary: '#FFFFFF',
+        },
+        business: {
+            primary: '#397CC2',
+            secondary: '#D3AC72',
+        },
+        select: {
+            main: '#848484',
+        },
+        chip: {
+            main: '#E5E5E5',
+        },
+        card: {
+            main: '#595959',
+        },
+    },
+    typography: {
+        fontSize: 14,
+        fontWeightLight: 300,
+        h1: {
+            fontSize: '2rem',
+            fontWeight: 700,
+            lineHeight: 1.2,
+            letterSpacing: '0.01em',
+        },
+        h2: {
+            fontSize: '1.5rem',
+            fontWeight: 700,
+        },
+        h3: {
+            fontSize: '1.2rem',
+            lineHeight: 1.2,
+            fontWeight: 700,
+        },
+        h4: {
+            fontSize: '1rem',
+            fontWeight: 500,
+            lineHeight: 1.2,
+        },
+        h5: {
+            fontSize: '0.8rem',
+        },
+        h6: {
+            fontSize: '0.6rem',
+        },
+        button: {
+            fontWeight: 700,
+            fontSize: '0.7rem',
+        },
+        body2: {
+            fontSize: '0.8rem',
+            lineHeight: 1.5,
+        },
+        subtitle1: {
+            fontWeight: 500,
+        },
+    },
+};
