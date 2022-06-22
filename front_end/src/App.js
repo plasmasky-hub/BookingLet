6b6d5b2f96564eb683525667443652aed534f419
wwwseven <wwwlc1996@gmail.com>
2022-06-22 16:29:47 +1000

    Add config file

---
+++ front_end/src/App.js
@@ -0,0 +1,16 @@
+// import LandingPage from "./pages/LandingPage";
+import React from 'react';
+import StoreDisplay from './pages/LandingPage/components/StoreDisplay';
+
+function App() {
+  return (
+    <StoreDisplay />
+    // <div>
+    //   <div>
+    //     <LandingPage />
+    //   </div>
+    // </div>
+  );
+}
+
+export default App;
