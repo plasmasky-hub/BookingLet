2c02a96e843197b1ae14e3b1e564b47a2c732aa6
Chris Wang <chris.wang@tcam.co>
2022-06-24 13:56:42 +1000

    optimize file structrue

--- front_end/src/pages/StoreListPage/index.jsx
+++ front_end/src/pages/StoreListPage/index.jsx
@@ -1,13 +1,1 @@
-import React from 'react';
-import StoreDisplay from './components/StoreDisplay';
-import Layout from '../../components/shared/Layout/Layout';
-
-const StoreListPage = () => {
-  return (
-    <Layout>
-      <StoreDisplay />
-    </Layout>
-  );
-};
-
-export default StoreListPage;
+export { default } from './StoreListPage';
