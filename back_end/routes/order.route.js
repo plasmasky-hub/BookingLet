165a62b036af95d06adbf16e2e71111824c1ab64
Jet Zhang <lovetrianglezym@hotmail.com>
2022-06-25 21:11:09 +1000

    change the cancel order route to the delete route

--- back_end/routes/order.route.js
+++ back_end/routes/order.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllOrders, addOrder, updateOrderByID, getOrderByID, deleteOrderByID,confirmOrder,cancelOrder,repeatVerification } = require('../controllers/order.controller');
+const { getAllOrders, addOrder, updateOrderByID, getOrderByID,confirmOrder,cancelOrder,repeatVerification } = require('../controllers/order.controller');
 
 const orderRouter = express.Router();
 
@@ -7,10 +7,10 @@ orderRouter.get('', getAllOrders);
 orderRouter.post('', addOrder);
 orderRouter.get('/:id', getOrderByID);
 orderRouter.put('/:id', updateOrderByID);
-orderRouter.delete('/:id', deleteOrderByID);
+orderRouter.delete('/:id',  cancelOrder);
 orderRouter.put('/:id/store', confirmOrder);
-orderRouter.put('/:id/user', cancelOrder);
-orderRouter.post('/serviceInfo', repeatVerification); //??
+// orderRouter.put('/:id/user', cancelOrder);
+orderRouter.post('/serviceInfo', repeatVerification); 
 
 
 module.exports = orderRouter;
