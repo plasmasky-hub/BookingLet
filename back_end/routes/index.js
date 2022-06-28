c78fc58cdfa44ab57e48b53f7980969c8c9f45f4
plasmasky-hub <61955808+plasmasky-hub@users.noreply.github.com>
2022-06-28 17:15:45 +0930

    delete routers

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -5,7 +5,6 @@ const serviceInfoRouter = require('./serviceInfo.route');
 const storeRouter = require('./store.route');
 const rootCategoryRouter = require('./rootCategory.route');
 const subCategoryRouter = require('./subCategory.route');
-const fakerRouter = require('./faker.route');
 
 const mainRouter = express.Router();
 
@@ -15,6 +14,5 @@ mainRouter.use('/store', storeRouter);
 mainRouter.use('/rootCategory', rootCategoryRouter);
 mainRouter.use('/subCategory', subCategoryRouter);
 mainRouter.use('/orders', orderRouter);
-mainRouter.use('/faker', fakerRouter);
 
 module.exports = mainRouter;
