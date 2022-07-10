ecd30cab8ad0ad041352bd28f89dd2f7c5194124
Jet Zhang <lovetrianglezym@hotmail.com>
2022-07-10 17:51:19 +1000

    check order time

--- back_end/controllers/order.controller.js
+++ back_end/controllers/order.controller.js
@@ -4,145 +4,75 @@ const User =require('../models/user');
 const Store =require('../models/store');
 const ServiceInfo =require('../models/serviceInfo')
 const { any } = require('joi');
+const { checkTimeIntervalAndBook}=require('./calendar.controller');
 
-async function checkTimeAvailability(orderDate,serviceId){
-  console.log('repeatedly verification...1');
-  // const {orderTime,serviceInfoId}=req.body;
-  const orderTimeDate=orderDate.date
-  let orderDay= new Date(orderTimeDate);
-  let dayInWeekIndex=orderDay.getDay();
-  let dayInWeek = undefined;
-  switch(dayInWeekIndex){
-    case 0:  dayInWeek ="Sunday"; break;
-    case 1:  dayInWeek ="Monday"; break;
-    case 2:  dayInWeek ="Tuesday"; break;
-    case 3:  dayInWeek ="Wednesday"; break;
-    case 4:  dayInWeek ="Thursday"; break;
-    case 5:  dayInWeek ="Friday"; break;
-    case 6:  dayInWeek ="Saturday"; break;
-  }
-  // return console.log(dayInWeek)
-// get open hours from serviceInfo time
-// console.log(id);
-const service = await ServiceInfo.findById(serviceId);
-const maxService=service.maxServicePerSection;
-const startTimeArr=service.startTime;
-
-let openHourArr= undefined;
-for(i=0;i<startTimeArr.length; i++){
-  if(startTimeArr[i].dayOfWeek=== dayInWeek){
-   openHourArr= startTimeArr[i].openHours;
-  }
-}
-//new Order Time 
-let orderTimeObjArr=openHourArr.map((element)=>{
-   return {date:orderDay,time:element}
-})
-
-let availableTime=[];
-for(j=0;j<orderTimeObjArr.length;j++){
-   orderCount = await Order.count({"orderTime":orderTimeObjArr[j],"serviceInfoId":serviceId})
-   availableTime.push({orderTime:orderTimeObjArr[j].time,availability:(orderCount< maxService)?true:false})
-}
-return availableTime;
-// return res.status(200).json(availableTime);
-
-}
-
-async function callCheckTimeAvailability(req,res){
-  console.log('repeatedly verification...2');
-  const {orderTime,serviceInfoId}=req.body;
-  const availableTime= await checkTimeAvailability(orderTime,serviceInfoId);
-  if (!availableTime) {
-    return res.status(404).json({ error });
-  }
-  return res.status(200).json(availableTime);
-
-  // const {orderTime,serviceInfoId}=req.body;
-  // const orderTimeDate=orderTime.date
-  // let orderDay= new Date(orderTimeDate);
-  // let dayInWeekIndex=orderDay.getDay();
-  // let dayInWeek = undefined;
-  // switch(dayInWeekIndex){
-  //   case 0:  dayInWeek ="Sunday"; break;
-  //   case 1:  dayInWeek ="Monday"; break;
-  //   case 2:  dayInWeek ="Tuesday"; break;
-  //   case 3:  dayInWeek ="Wednesday"; break;
-  //   case 4:  dayInWeek ="Thursday"; break;
-  //   case 5:  dayInWeek ="Friday"; break;
-//     case 6:  dayInWeek ="Saturday"; break;
-//   }
-// // get open hours from serviceInfo time
-// const service = await ServiceInfo.findById(serviceInfoId).exec();
-// const maxService=service.maxServicePerSection;
-// const startTimeArr=service.startTime;
-
-// let openHourArr= undefined;
-// for(i=0;i<startTimeArr.length;i++){
-//   if(startTimeArr[i].dayOfWeek=== dayInWeek){
-//    openHourArr= startTimeArr[i].openHours;
-//   }
-// }
-// //new Order Time 
-// let orderTimeObjArr=openHourArr.map((element)=>{
-//    return {date:orderDay,time:element}
-// })
-
-// let availableTime=[];
-// for(j=0;j<orderTimeObjArr.length;j++){
-//    b = await Order.count({"orderTime":orderTimeObjArr[j],"serviceInfoId":serviceInfoId})
-//    availableTime.push({orderTime:orderTimeObjArr[j].time,availability:(b< maxService)?true:false})
-// }
-// res.status(200).json(availableTime);
-}
 
 //create order
-async function addOrder(req, res){
-    console.log('Adding a new order...');
-    const { peopleNumber, orderTime, bookingStatus,cancelStatus,userId,storeId,serviceInfoId,tel,optionInfo,bookingTime  } = req.body;
-    const newOrder = new Order({ peopleNumber, orderTime, bookingStatus,cancelStatus,userId,storeId,serviceInfoId,tel,optionInfo,bookingTime  });
-    const orderId =newOrder._id;
-    const serviceId = newOrder.serviceInfoId;
-    const newOrderTime =newOrder.orderTime;
-    const usersId=newOrder.userId;
-    //get orders with the same services ID and same order time
-    const userExist =await Order.find({"orderTime":newOrderTime ,"userId":usersId,"serviceInfoId":serviceId,})
-    //Object.key(orders).length ===0  JSON.stringify(orders)==='{}'
-    if(JSON.stringify(userExist)!=='[]'){
-      return res.status(200).json('You have already booked the same time, pleas check your order list')
-     }
+async function addOrder(req,res){
+  const { peopleNumber, orderTime,userId,serviceInfoId,tel,optionInfo} = req.body;
+  const bookingTime =new Date(orderTime.date)
+  const serviceInfo =await ServiceInfo.findById(serviceInfoId).exec();
+  const storeId =serviceInfo.store;
 
-    //check Service Number
-     //get the number of max Services 
-    //  const services = await ServiceInfo.findById(serviceId);
-    //  const maxServicesNumber= services.maxServicePerSection;
-    //  console.log(maxServicesNumber);
-    //  //Already have number
-    //  const existServicesNumber = await Order.count({"orderTime":newOrderTime ,"serviceInfoId":serviceId})
-    //  console.log(existServicesNumber)
 
-    //  if(existServicesNumber +1 > maxServicesNumber){
-    //   return res.status(200).json('Booking number is  full, please find another time')
-    //  }
+//slice time
+  let startHour = parseInt(orderTime.startTime);
+  let endHour = parseInt(orderTime.endTime);
+  let timeSliceArr = [];
+    for (let i = startHour; i < endHour; i += 5) {
+        if (i % 100 < 60) { timeSliceArr.push(i); };
+    }
+
+  const orderArr = await Order.find({"orderTime.date":orderTime.date,"serviceInfoId":serviceInfoId,"userId":userId}).exec();
+  let permission=true;
+
+  for(let i =0; i< orderArr.length; i++){
+    let comparedTimeSliceArr = [];
+    let comparedStartHour = parseInt(orderArr[i].orderTime.startTime);
+    let comparedEndHour = parseInt(orderArr[i].orderTime.endTime);
+
+    for (let j =  comparedStartHour; j < comparedEndHour; j += 5) {
+        if (j % 100 < 60) { comparedTimeSliceArr.push(j); };
+    }
+    let repeatedTimeSlice= comparedTimeSliceArr.filter((element)=>{
+      return timeSliceArr.indexOf(element)> -1 
+    })
+    if ( repeatedTimeSlice.length>0){
+      permission= false;
+    }
+  }
+
+
+  // res.json(comparedTimeSliceArr);
+
+  
+
+  
+  // const user =await User.findById(userId).exec();
+  //   if(user.orders.indexOf(orderId) !== -1){
+  //     return res.status(400).json({ error: 'Order already exists' });}
+  //     else{
+  //   user.orders.addToSet(orderId);
+  //   await user.save();}
    
-    // add orderId into User collection
-
-    const user =await User.findById(userId).exec();
-    if(user.orders.indexOf(orderId) !== -1){
-      return res.status(400).json({ error: 'Order already exists' });}
-      else{
-    user.orders.addToSet(orderId);
-    await user.save();}
-   
-    //add orderId into Store collection
-    const store = await Store.findById(storeId).exec();
-    store.orders.addToSet(orderId);
-    await store.save();
-
+  //   //add orderId into Store collection
+  //   const store = await Store.findById(storeId).exec();
+  //   store.orders.addToSet(orderId);
+  //   await store.save();
+if(permission){
+  [req.body.date,req.body.startHour,res.body.endHour]=[
+    req.body.orderTime.date,req.body.orderTime.startTime,req.body.orderTime.endTime]
+    const newOrder = new Order({ peopleNumber, orderTime,userId,storeId,serviceInfoId,tel,optionInfo,bookingTime });
     await newOrder.save();
-    res.status(201).json(newOrder);
-    // res.status(200).json({data:newOrder});
+
+    await checkTimeIntervalAndBook(req,res);
+    
+}else{
+     res.send("You have made an appointment at same time, please select another time. ")
 }
+}
+
+
 
 //update
 async function updateOrderByID(req,res){
@@ -232,9 +162,9 @@ module.exports = {
     updateOrderByID,
     // deleteOrderByID,
     confirmOrder,
-    cancelOrder,
-    checkTimeAvailability,
-    callCheckTimeAvailability
+    cancelOrder
+    
+ 
 
    
 }
