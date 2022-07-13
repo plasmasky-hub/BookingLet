aea1a6566f97de59d8dba911321863c427e51151
wwwseven <wwwlc1996@gmail.com>
2022-07-14 00:25:54 +1000

    fix banner

--- front_end/src/pages/BookingPage/components/Banner.jsx
+++ front_end/src/pages/BookingPage/components/Banner.jsx
@@ -1,17 +1,12 @@
-import React from 'react';
 import styled from '@emotion/styled';
 import food from '../../../assets/food.jpg';
 
-const BannerWrapper = styled.div`
+const Banner = styled.div`
   width: 100%;
   height: 256px;
   background-image: url(${food});
   background-size: cover;
   border-radius: 10px;
 `;
-
-const Banner = () => {
-  return <BannerWrapper></BannerWrapper>;
-};
 
 export default Banner;
