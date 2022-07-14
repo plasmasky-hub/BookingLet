6bd363e0e025a4aaedd2f4ed58f314a1e2961c48
Harry <harry515952831@gmail.com>
2022-07-15 01:00:51 +1000

    Conflict

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,8 +3,6 @@ import LandingPage from './pages/LandingPage';
 import StoreListPage from './pages/StoreListPage';
 import BookingPage from './pages/BookingPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import StoreInfPage from "./pages/LandingPage/components/StoreInformation";
-
 function App() {
   return (
     <BrowserRouter>
@@ -13,7 +11,6 @@ function App() {
         <Route path="/LandingPage" element={<LandingPage />}></Route>
         <Route path="/StoreListPage" element={<StoreListPage />}></Route>
         <Route path="/BookingPage/:_id" element={<BookingPage />}></Route>
-        <Route path="/StoreInfPage" element={<StoreInfPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
