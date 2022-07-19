afe85cd654270e313b37de2711d2982f602e4c0d
RedRe4per <s3582474@gmail.com>
2022-07-19 21:46:42 +1000

    small bug fixed

--- back_end/controllers/order.controller.js
+++ back_end/controllers/order.controller.js
@@ -24,7 +24,7 @@ async function addOrder(req, res) {
   //slice time
   let startHour = parseInt(orderTime.startTime);
   let endHour = null;
-  if (orderTime.endHour === undefined) {
+  if (orderTime.endTime === undefined) {
     endHour = startHour + 5;
     req.body.orderTime.endTime = startHour + 5;
   } else {
@@ -38,7 +38,6 @@ async function addOrder(req, res) {
       timeSliceArr.push(i);
     }
   }
-  console.log(timeSliceArr)
 
   const orderArr = await Order.find({
     'orderTime.date': orderTime.date,
