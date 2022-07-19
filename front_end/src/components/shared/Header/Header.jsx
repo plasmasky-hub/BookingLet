b9a297359906988f0c2d8b05816dcba0a859ecd2
plasmasky-hub <61955808+plasmasky-hub@users.noreply.github.com>
2022-07-19 17:11:32 +0930

    login静态完善

--- front_end/src/components/shared/Header/Header.jsx
+++ front_end/src/components/shared/Header/Header.jsx
@@ -1,35 +1,17 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { useState } from 'react';
-
 import { Logo } from '../../shared/Logo/Logo';
 import { UserPanel } from './UserPanel';
-import ModalUnstyled from '@mui/base/ModalUnstyled'
-import loginImg from '../../../assets/loginImg.png';
+import { LoginModal } from '../Login/Login';
 
 import LoginIcon from '@mui/icons-material/Login';
-import { 
-	Visibility,
-	VisibilityOff,
-} from '@mui/icons-material';
 
 import { 
 	Box, 
 	Button, 
-	FormControl, 
-	Grid, 
-	Input, 
-	InputBase, 
-	InputLabel, 
-	TextField, 
-	Modal, 
-	Typography,
-	OutlinedInput,
-	InputAdornment,
-	IconButton,
-	FormHelperText,
-	
 } from '@mui/material';
+import { useRef } from 'react';
 
 
 const StyledHeader = styled(Box)`
@@ -84,70 +66,14 @@ const RegisterButton = styled(Button)`
 `;
 
 
-const loginFormStyle = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	justifyContent: 'center',
-	alignItems: 'center',
-	transform: 'translate(-50%, -50%)',
-	padding: '0px 0px 0px 0px',
-
-	bgcolor: 'background.paper',
-	borderRadius: '25px',
-	boxShadow: 24,
-	p: 4,
-};
-
-const LoginImage = styled(Typography)`
-	justifyContent: center;
-	alignItems: center;
-	font-size: 0.6rem;
-
-	img{
-		height: 420px;
-		width: 250px;
-	}
-` 
-
-const TextFieldStyle = {
-	mt: 4, 
-	width: 350,
-}
-
-const LoginButtonStyle = {
-	mt: 4,
-	width: 200
-}
-
 export const Header = () => {
 	const [loggedIn, setLoggedIn] = useState(false);
-	const [loginIsOpen, setLogin] = React.useState(false);
-	const loginOpen = () => setLogin(true);
-	const loginClose = () => setLogin(false);
+	const [loginIsOpen, setIsOpen] = useState(false);
 
-	const [values, setValues] = React.useState({
-		amount: '',
-		password: '',
-		weight: '',
-		weightRange: '',
-		showPassword: false,
-	});
-
-	const handleChange = (prop) => (event) => {
-		setValues({ ...values, [prop]: event.target.value });
-	};
-
-	const handleClickShowPassword = () => {
-		setValues({
-		...values,
-		showPassword: !values.showPassword,
-		});
-	};
-
-	const handleMouseDownPassword = (event) => {
-		event.preventDefault();
-	};
+	const loginOpen = () => {
+		setIsOpen(true);
+		// this.LoginModal.loginOpen();
+	}
 
   return (
     <StyledHeader>
@@ -159,69 +85,12 @@ export const Header = () => {
 			<StyledLoginButton 
 				variant="text" 
 				startIcon={<LoginIcon />}
-				onClick={loginOpen}>
+				// onClick={() => loginOpen()}>
+				onClick={() => loginOpen()}>
 				Log in
 			</StyledLoginButton>
-			
-			<Modal
-				open={loginIsOpen}
-				onClose={loginClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description"
-				name = "loginModal"
-			>
-				<Box sx={loginFormStyle}>
-					<Grid container spacing={0}>
-						<Grid item xs={5}>
-							<LoginImage>
-								<img src={loginImg}></img>
-							</LoginImage>
-						</Grid>
-						
-						<Grid item xs={7}>
-							<Typography id="modal-modal-title" variant="h4" component="h2">
-								Welcome to Bookinglet!
-							</Typography>
 
-							<TextField variant='outlined' id="account-basic" label="Email" color='success' sx={TextFieldStyle} />
-
-							<FormControl sx={TextFieldStyle} color='success'>
-								<InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-								<OutlinedInput
-									id="outlined-adornment-password"
-						
-									type={values.showPassword ? 'text' : 'password'}
-									value={values.password}
-									onChange={handleChange('password')}
-									endAdornment={
-									<InputAdornment position="end">
-										<IconButton
-										aria-label="toggle password visibility"
-										onClick={handleClickShowPassword}
-										onMouseDown={handleMouseDownPassword}
-										edge="end"
-										>
-										{values.showPassword ? <VisibilityOff /> : <Visibility />}
-										</IconButton>
-									</InputAdornment>
-									}
-									label="Password"
-								/>
-							</FormControl>
-
-							<Button 
-								variant='contained'
-								color='success'
-								sx={LoginButtonStyle}
-							>Sign in</Button>
-
-								
-						</Grid>
-						
-					</Grid>
-					
-				</Box>
-          	</Modal>
+			<LoginModal open={loginIsOpen}/>
 
 		  	<RegisterButton variant="contained">Register</RegisterButton>
         </ButtonWrapper>
---
+++ front_end/src/components/shared/Login/Login.jsx
@@ -0,0 +1,193 @@
+import styled from '@emotion/styled';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import loginImg from '../../../assets/loginImg.png';
+import { 
+	Visibility,
+	VisibilityOff,
+} from '@mui/icons-material';
+import CloseIcon from '@mui/icons-material/Close';
+
+import { 
+	Box, 
+	Button, 
+	FormControl, 
+	Grid, 
+	InputLabel, 
+	TextField, 
+	Modal, 
+	Typography,
+	OutlinedInput,
+	InputAdornment,
+	IconButton,
+    Link,
+
+} from '@mui/material';
+
+const loginFormStyle = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	justifyContent: 'center',
+	alignItems: 'center',
+	transform: 'translate(-50%, -50%)',
+	padding: '0px 0px 0px 0px',
+
+	bgcolor: 'background.paper',
+	borderRadius: '25px',
+	boxShadow: 24,
+	p: 4,
+};
+
+const LoginImage = styled(Typography)`
+	font-size: 0.6rem;
+
+	img{
+		height: 100%;
+		width: 100%;
+	}
+` 
+
+const TextFieldStyle = {
+	mt: 3, 
+	width: '100%',
+}
+
+const LoginButtonStyle = {
+	mt: 4,
+	width: 200
+}
+
+export const LoginModal = (props) => {
+    
+    var isOpen = props.open;
+    const [loginIsOpen, setLogin] = React.useState(false);
+	const loginOpen = () => setLogin(true);
+	// const loginClose = () => setLogin(false);
+    const loginClose = () => {
+        isOpen = false;
+        setLogin(false);
+    }
+
+    const handleRegisterOpen = () => {
+
+    }
+
+    // componentDidMount(){
+    //     // 将子组件指向父组件的变量
+    //     this.props.onRef && this.props.onRef(this);
+    // }
+
+    useEffect( () => isOpen ? loginOpen() : loginClose());
+
+	const [values, setValues] = React.useState({
+		amount: '',
+		password: '',
+		weight: '',
+		weightRange: '',
+		showPassword: false,
+	});
+
+	const handleChange = (prop) => (event) => {
+		setValues({ ...values, [prop]: event.target.value });
+	};
+
+	const handleClickShowPassword = () => {
+		setValues({
+		...values,
+		showPassword: !values.showPassword,
+		});
+	};
+
+	const handleMouseDownPassword = (event) => {
+		event.preventDefault();
+	};
+
+    return (
+        <Modal
+            open={loginIsOpen}
+            onClose={loginClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            name = "loginModal"
+        >
+            <Box sx={loginFormStyle}>
+                <Grid container spacing={1.5}>
+                    <Grid item xs={5}>
+                        <LoginImage>
+                            <img src={loginImg}></img>
+                        </LoginImage>
+                    </Grid>
+                    
+                    <Grid item xs={7}>
+                        <Box sx={{ width: '100%' , display: 'flex', justifyContent: 'end'}}>
+                            <IconButton aria-label="close" >
+                                <CloseIcon onClick={loginClose}/>
+                            </IconButton>
+                        </Box>
+
+                        <Typography id="modal-modal-title" variant="h5" component="h5">
+                            Welcome to Bookinglet!
+                        </Typography>
+
+                        <TextField variant='outlined' id="account-basic" label="Email" color='success' sx={TextFieldStyle} />
+
+                        <FormControl sx={TextFieldStyle} color='success'>
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                    
+                                type={values.showPassword ? 'text' : 'password'}
+                                value={values.password}
+                                onChange={handleChange('password')}
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                    >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                                label="Password"
+                            />
+                        </FormControl>
+
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
+                            <Button 
+                                variant='contained'
+                                color='success'
+                                sx={LoginButtonStyle}
+                            >Sign in</Button>
+                        </Box>
+
+                        <Grid container spacing={1} 
+                            direction='row' 
+                            justifyContent='center' 
+                            sx={{ mt: 2, fontStyle: 'italic'}}>
+                            <Grid item>
+                                <Typography > Not a member yet? </Typography> 
+                            </Grid>
+                            <Grid item>
+                                <Link onClick={handleRegisterOpen} color='inherit'> Register </Link> 
+                            </Grid>
+                            <Grid item>
+                                <Typography > now!</Typography> 
+                            </Grid>
+                        </Grid>
+                        
+
+                            
+                    </Grid>
+                    
+                </Grid>
+                
+            </Box>
+        </Modal>
+    )
+
+   
+}
