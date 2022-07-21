ac73d7f9e58b6177666eb9925c8253116a971138
plasmasky-hub <61955808+plasmasky-hub@users.noreply.github.com>
2022-07-21 13:54:51 +0930

    add formik

--- front_end/src/components/shared/Login/Login.jsx
+++ front_end/src/components/shared/Login/Login.jsx
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled';
 import React, { useEffect } from 'react';
-import { useState } from 'react';
+import { useFormik } from 'formik';
 import loginImg from '../../../assets/loginImg.png';
 import { 
 	Visibility,
 	VisibilityOff,
 } from '@mui/icons-material';
-import CloseIcon from '@mui/icons-material/Close';
 
 import { 
 	Box, 
@@ -15,14 +14,11 @@ import {
 	Grid, 
 	InputLabel, 
 	TextField, 
-	Modal, 
 	Typography,
 	OutlinedInput,
 	InputAdornment,
 	IconButton,
     Link,
-    Form,
-
 } from '@mui/material';
 
 const loginFormStyle = {
@@ -40,178 +36,171 @@ const loginFormStyle = {
 	p: 4,
 };
 
-const LoginImage = styled(Typography)`
+const LoginModalBox = styled(Box)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    justify-content: center;
+    align-items: center;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    border-radius: 25px;
+    box-shadow: 24px;
+`
+
+const LoginImage = styled(Box)`
 	font-size: 0.6rem;
+    height: 100%;
+    width: 100%;
 
 	img{
 		height: 100%;
 		width: 100%;
+        object-fit: cover;
+        object-position: left;
+        border-top-left-radius: 25px;
+        border-bottom-left-radius: 25px;
 	}
 ` 
+
+const LoginInputField = styled(TextField)`
+    margin-top: 3;
+    width: 100%;
+`
 
 const TextFieldStyle = {
 	mt: 3, 
 	width: '100%',
 }
 
-const LoginButtonStyle = {
-	mt: 4,
-	width: 200
-}
+const LoginButton = styled(Button)`
+    margin-top: 32px;
+	width: 200px;
+`
 
 export const LoginModal = (props) => {
-    
-    var isOpen = props.open;
-    const [loginIsOpen, setLogin] = React.useState(false);
-	const loginOpen = () => setLogin(true);
-	// const loginClose = () => setLogin(false);
-    const loginClose = () => {
-        isOpen = false;
-        setLogin(false);
-    }
+
+    const handleClickShowPassword = () => {
+		formik.setValues({
+		...formik.values,
+		showPassword: !formik.values.showPassword,
+		});
+	};
+
+    const formik = useFormik({
+        initialValues: {
+          email: 'foobar@example.com',
+          password: 'foobar',
+          showPassword: false,
+        },
+        handleClickShowPassword: handleClickShowPassword,
+        // validationSchema: validationSchema,
+        onSubmit: (values) => {
+          alert(JSON.stringify(values, null, 2));
+        },
+      });
 
     const handleRegisterOpen = () => {
 
     }
 
-    // componentDidMount(){
-    //     // 将子组件指向父组件的变量
-    //     this.props.onRef && this.props.onRef(this);
-    // }
-
-    useEffect( () => isOpen ? loginOpen() : loginClose());
-
-	const [values, setValues] = React.useState({
-		email: '',
-		password: '',
-		weight: '',
-		weightRange: '',
-		showPassword: false,
-	});
-
-	const handleChange = (prop) => (event) => {
-		setValues({ ...values, [prop]: event.target.value });
-	};
-
-	const handleClickShowPassword = () => {
-		setValues({
-		...values,
-		showPassword: !values.showPassword,
-		});
-	};
+	// const [values, setValues] = React.useState({
+	// 	email: '',
+	// 	password: '',
+	// 	weight: '',
+	// 	weightRange: '',
+	// 	showPassword: false,
+	// });
 
 	const handleMouseDownPassword = (event) => {
 		event.preventDefault();
 	};
 
-    const handleSubmit = (event) => {
-        const { email } = values.email;
-        const { password } = values.password;
-
-        event.preventDefault();
-        console.log( 'Email:', email, 'Password: ', password); 
-        alert('Submitted! ' + email + password);
-    }
-
     return (
-        // <Modal
-        //     open={loginIsOpen}
-        //     onClose={loginClose}
-        //     aria-labelledby="modal-modal-title"
-        //     aria-describedby="modal-modal-description"
-        //     name = "loginModal"
-        // >
-            <Box sx={loginFormStyle}>
-                <Grid container spacing={1.5}>
-                    <Grid item xs={5}>
-                        <LoginImage>
-                            <img src={loginImg}></img>
-                        </LoginImage>
-                    </Grid>
-                    
-                    <Grid item xs={7}>
-                        <Box sx={{ width: '100%' , display: 'flex', justifyContent: 'end'}}>
-                            <IconButton aria-label="close" >
-                                <CloseIcon onClick={loginClose}/>
-                            </IconButton>
-                        </Box>
 
-                        <Typography id="modal-modal-title" variant="h5" component="h5">
-                            Welcome to Bookinglet!
-                        </Typography>
+        <LoginModalBox >
+            <Grid container spacing={0} alignItems="stretch">
+                <Grid item xs={5} >
+                    <LoginImage>
+                        <img src={loginImg}></img>
+                    </LoginImage>
+                </Grid>
+                
+                <Grid item xs={7} sx={{ padding: 3 }}>
+                    <Typography id="modal-modal-title" variant="h5" component="h5">
+                        Welcome to Bookinglet!
+                    </Typography>
 
-                        <Box component='form' onSubmit={handleSubmit}>
-                            <TextField 
-                                variant='outlined' 
-                                id="account-basic" 
-                                label="Email" 
-                                color='success' 
-                                value={values.email}
-                                onChange={handleChange('email')}
-                                sx={TextFieldStyle} />
+                    <Box component='form' onSubmit={formik.handleSubmit}>
 
-                            <FormControl color='success' sx={TextFieldStyle}>
-                                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                                <OutlinedInput
-                                    id="outlined-adornment-password"
+                        <LoginInputField 
+                            variant='outlined' 
+                            label="Email" 
+                            name='email'
+                            color='success' 
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email)}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
 
-                                    type={values.showPassword ? 'text' : 'password'}
-                                    value={values.password}
-                                    onChange={handleChange('password')}
-                                    endAdornment={
+                        <FormControl color='success' sx={TextFieldStyle}>
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                label="Password"
+                                name='password'
+                                type={formik.values.showPassword ? 'text' : 'password'}
+                                value={formik.values.password}
+                                onChange={formik.handleChange}
+                                endAdornment={
                                     <InputAdornment position="end">
                                         <IconButton
                                         aria-label="toggle password visibility"
+                                        name='showPassword'
                                         onClick={handleClickShowPassword}
                                         onMouseDown={handleMouseDownPassword}
                                         edge="end"
                                         >
-                                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                        {formik.values.showPassword ? <VisibilityOff /> : <Visibility />}
                                         </IconButton>
                                     </InputAdornment>
-                                    }
-                                    label="Password"
-                                />
-                            </FormControl>
+                                }
+                            />
+                        </FormControl>
 
-                            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
-                                <Button 
-                                    variant='contained'
-                                    color='success'
-                                    type="submit"
-                                    sx={LoginButtonStyle}
-                                    // onClick={handleSubmit}
-                                >Sign in</Button>
-                            </Box>
+                        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center'}}>
+                            <LoginButton 
+                                variant='contained'
+                                color='success'
+                                type="submit"
+                            >
+                                Sign in
+                            </LoginButton>
                         </Box>
+                    </Box>
                         
 
                         
 
-                        <Grid container spacing={1} 
-                            direction='row' 
-                            justifyContent='center' 
-                            sx={{ mt: 2, fontStyle: 'italic'}}>
-                            <Grid item>
-                                <Typography > Not a member yet? </Typography> 
-                            </Grid>
-                            <Grid item>
-                                <Link onClick={handleRegisterOpen} color='inherit'> Register </Link> 
-                            </Grid>
-                            <Grid item>
-                                <Typography > now!</Typography> 
-                            </Grid>
+                    <Grid container spacing={1} 
+                        direction='row' 
+                        justifyContent='center' 
+                        sx={{ mt: 2, fontStyle: 'italic'}}>
+                        <Grid item>
+                            <Typography > Not a member yet? </Typography> 
                         </Grid>
-                        
-
-                            
+                        <Grid item>
+                            <Link onClick={handleRegisterOpen} color='inherit'> Register </Link> 
+                        </Grid>
+                        <Grid item>
+                            <Typography > now!</Typography> 
+                        </Grid>
                     </Grid>
-                    
+                            
                 </Grid>
-                
-            </Box>
-        // {/* </Modal> */}
+                    
+            </Grid>
+            
+        </LoginModalBox>
     )
-
-   
 }
