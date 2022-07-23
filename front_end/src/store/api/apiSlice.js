91f7534aada072f3ebf431e442b367a68384efd0
xinglin chen <austinchen666@gmail.com>
2022-07-23 15:02:28 +1000

    comment antd

--- front_end/src/store/api/apiSlice.js
+++ front_end/src/store/api/apiSlice.js
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'bookinglet-app-load-balancer-792262709.ap-southeast-2.elb.amazonaws.com/v1/' }),
   endpoints: () => ({}),
 });
