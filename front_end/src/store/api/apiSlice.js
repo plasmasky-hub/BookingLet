2126cbc4742f8c4a96bdc40de7cd43c767131bc7
xinglin chen <austinchen666@gmail.com>
2022-07-23 18:25:51 +1000

    update baseURL

--- front_end/src/store/api/apiSlice.js
+++ front_end/src/store/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://www.api.uat.tin6.online/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.uat.tin6.online/v1/' }),
   endpoints: () => ({}),
 });
 
