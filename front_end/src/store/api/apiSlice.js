05441a2085c395f41ff793b4a7f5f55db2711a5c
Xinglin Chen <49745258+xinglinchen666@users.noreply.github.com>
2022-07-25 13:43:10 +1000

    Update apiSlice.js

--- front_end/src/store/api/apiSlice.js
+++ front_end/src/store/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/v1' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://api.uat.bookinglet.com.au/v1' }),
   tagTypes: ['Service'],
   endpoints: () => ({}),
 });
