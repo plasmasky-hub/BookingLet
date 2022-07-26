1c0e0435e66b9f11d5be5940d41c7fc3a8363c36
Anita-XR-LIU <scorpioliu1108@gmail.com>
2022-07-26 22:02:29 +0930

    format

--- front_end/src/pages/UserBookingPage/components/BookingHistory/BookingHistory.jsx
+++ front_end/src/pages/UserBookingPage/components/BookingHistory/BookingHistory.jsx
@@ -1,56 +1,64 @@
-import styled from '@emotion/styled';
-import { useState } from 'react';
-import { Container, Chip, Stack } from '@mui/material';
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import { BookingTable } from '../BookingTable/BookingTable';
-import { useGetOrdersQuery, useUpdateOrderQuery } from '../../../../store/api/orderApi';
+import styled from "@emotion/styled";
+import { useState } from "react";
+import { Container, Chip, Stack } from "@mui/material";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
+import { BookingTable } from "../BookingTable/BookingTable";
+import {
+  useGetOrdersQuery,
+  useUpdateOrderQuery,
+} from "../../../../store/api/orderApi";
 
 const BookingPageWrapper = styled(Container)`
-    width: 100%;
-    height: 100%;
-    padding: 20px 30px;
-    background-color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 20px 30px;
+  background-color: #fff;
 `;
 
 const StyledChips = styled(Chip)`
-    height: 22px;
-    padding: 0 10px;
-    font-size: 0.7rem;
-    text-align: center;
-    align-items: center;
-    cursor: pointer;
-    transition: ease-in-out 0.4s;
+  height: 22px;
+  padding: 0 10px;
+  font-size: 0.7rem;
+  text-align: center;
+  align-items: center;
+  cursor: pointer;
+  transition: ease-in-out 0.4s;
 `;
 
-const ChipBackground = '#7B8B6F';
+const ChipBackground = "#7B8B6F";
 
-const chipItems = ['All', 'Uncomfirmed', 'Confirmed', 'Done'];
+const chipItems = ["All", "Uncomfirmed", "Confirmed", "Done"];
 
 export const BookingHistory = () => {
-    const { data, isSuccess } = useGetOrdersQuery();
-    const orders = isSuccess && data;
-    const [clicked, setClicked] = useState(ChipBackground);
-    const statusIndicator = chipItems.map((chipItems) =>
-        <StyledChips
-            label={chipItems}
-            sx={{ color: `${ChipBackground[clicked]}` }}
-            onClick={() => {
-                setClicked(true)
-            }}></StyledChips>)
-    return (
-        <BookingPageWrapper>
-            <h2>My Bookings</h2>
-            <Stack direction="row" spacing={1}>
-                <FilterAltOutlinedIcon sx={{ mr: 1 }} />
-                <span><strong>Filter by booking status</strong></span>
-            </Stack>
-            <Stack direction="row" spacing={1} sx={{ py: 2 }}>
-                {statusIndicator}
-            </Stack>
-            <BookingTable />
-            <BookingTable />
-            <BookingTable />
-            <BookingTable />
-        </BookingPageWrapper>
-    );
+  const { data, isSuccess } = useGetOrdersQuery();
+  const orders = isSuccess && data;
+
+  const [clicked, setClicked] = useState(ChipBackground);
+  const statusIndicator = chipItems.map((chipItems) => (
+    <StyledChips
+      label={chipItems}
+      sx={{ color: `${ChipBackground[clicked]}` }}
+      onClick={() => {
+        setClicked(ChipBackground);
+      }}
+    ></StyledChips>
+  ));
+  return (
+    <BookingPageWrapper>
+      <h2>My Bookings</h2>
+      <Stack direction="row" spacing={1}>
+        <FilterAltOutlinedIcon sx={{ mr: 1 }} />
+        <span>
+          <strong>Filter by booking status</strong>
+        </span>
+      </Stack>
+      <Stack direction="row" spacing={1} sx={{ py: 2 }}>
+        {statusIndicator}
+      </Stack>
+      <BookingTable />
+      <BookingTable />
+      <BookingTable />
+      <BookingTable />
+    </BookingPageWrapper>
+  );
 };
--- front_end/src/pages/UserBookingPage/components/BookingTable/BookingTable.jsx
+++ front_end/src/pages/UserBookingPage/components/BookingTable/BookingTable.jsx
@@ -1,7 +1,15 @@
-import styled from '@emotion/styled';
-import { Box, TableRow, TableCell, Paper, Collapse, ButtonGroup, Button } from '@mui/material';
-import { useState } from 'react';
-import BodyRelaxing from '../../../../assets/BodyRelaxing.png';
+import styled from "@emotion/styled";
+import {
+  Box,
+  TableRow,
+  TableCell,
+  Paper,
+  Collapse,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+import BodyRelaxing from "../../../../assets/BodyRelaxing.png";
 
 const StyledTableContainer = styled(Box)`
   min-width: 800px;
@@ -12,41 +20,32 @@ const StyledTableContainer = styled(Box)`
   align-items: center;
   background-color: #f0f0f0;
   margin-bottom: 8px;
-`
+`;
 
 const TableCellImg = styled.img`
   width: 150px;
   height: 100px;
-`
+`;
 
 const TableHead = {
-  name: 'Facial',
-  date: 'Date',
-  time: 'Time',
-  status: 'Status'
+  name: "Facial",
+  date: "Date",
+  time: "Time",
+  status: "Status",
 };
 
 const TableFoot = {
-  address: 'Adelaide',
-  date: '18/06/2022',
-  time: '13:00 - 14:00',
-  status: ['Confirmed', 'Unconfirmed', 'Cancelled'],
+  address: "Adelaide",
+  date: "18/06/2022",
+  time: "13:00 - 14:00",
+  status: ["Confirmed", "Unconfirmed", "Cancelled"],
 };
 
-const StatusColor = ['#7B8B6F', '#CEA02C', '#D76D6D'];
+const StatusColor = ["#7B8B6F", "#CEA02C", "#D76D6D"];
 
 const StyledButtonGroup = styled(ButtonGroup)`
   width: 150px;
   height: 25px;
-  /* border-radius: 10px;
-  color: #fff;
-  background-color: #f0f0f0;
-  text-align: center;
-  cursor: pointer;
-  &:hover {
-    font-weight: 600;
-    transition: ease-out 0.2s;
-  } */
 `;
 
 const ConfirmButton = styled(Button)`
@@ -81,7 +80,7 @@ const CollapsedTitle = styled(Box)`
 const CollapsedNotice = styled(Box)`
   width: 100%;
   font-style: italic;
-  color: #D76D6D;
+  color: #d76d6d;
   background-color: #fff;
   padding: 0 0 5px 40px;
 `;
@@ -97,79 +96,108 @@ export const BookingTable = () => {
 
   return (
     <>
-    <StyledTableContainer component={Paper}>
-      <TableRow direction='column' sx={{ width: '1000px' }}>
-        <TableCell sx={{ px: 3 }}>
-          <TableCellImg src={BodyRelaxing} />
-        </TableCell>
-        <TableCell sx={{ pr: 8 }}>
-          <h4>{TableHead.name}</h4>
-          <p>{TableFoot.address}</p>
-        </TableCell>
-        <TableCell sx={{ pr: 8 }}>
-          <h4>{TableHead.date}</h4>
-          <p>{TableFoot.date}</p>
-        </TableCell>
-        <TableCell sx={{ pr: 8 }}>
-          <h4>{TableHead.time}</h4>
-          <p>{TableFoot.time}</p>
-        </TableCell>
-        <TableCell sx={{ pr: 8 }}>
-          <h4>{TableHead.status}</h4>
-            <p style={{color: `${StatusColor[statusIndex]}`}}>
-            {TableFoot.status[statusIndex]}
-          </p>
-        </TableCell>
-          <TableCell sx={{ cursor: 'pointer' }}>
+      <StyledTableContainer component={Paper}>
+        <TableRow direction="column" sx={{ width: "1000px" }}>
+          <TableCell sx={{ px: 3 }}>
+            <TableCellImg src={BodyRelaxing} />
+          </TableCell>
+          <TableCell sx={{ pr: 8 }}>
+            <h4>{TableHead.name}</h4>
+            <p>{TableFoot.address}</p>
+          </TableCell>
+          <TableCell sx={{ pr: 8 }}>
+            <h4>{TableHead.date}</h4>
+            <p>{TableFoot.date}</p>
+          </TableCell>
+          <TableCell sx={{ pr: 8 }}>
+            <h4>{TableHead.time}</h4>
+            <p>{TableFoot.time}</p>
+          </TableCell>
+          <TableCell sx={{ pr: 8 }}>
+            <h4>{TableHead.status}</h4>
+            <p style={{ color: `${StatusColor[statusIndex]}` }}>
+              {TableFoot.status[statusIndex]}
+            </p>
+          </TableCell>
+          <TableCell sx={{ cursor: "pointer" }}>
             <p onClick={handleClick}>Order detail</p>
-            <StyledButtonGroup variant="contained" aria-label="outlined primary button group">
-              {statusIndex === 1 ? <ConfirmButton onClick={() => {
-                setStatusIndex(0) 
-              }}>Confirm</ConfirmButton> : <ConfirmButton onClick={() => {
-                setStatusIndex(0) 
-              }} disabled>Confirm</ConfirmButton>}
+            <StyledButtonGroup
+              variant="contained"
+              aria-label="outlined primary button group"
+            >
+              {statusIndex === 1 ? (
+                <ConfirmButton
+                  onClick={() => {
+                    setStatusIndex(0);
+                  }}
+                >
+                  Confirm
+                </ConfirmButton>
+              ) : (
+                <ConfirmButton
+                  onClick={() => {
+                    setStatusIndex(0);
+                  }}
+                  disabled
+                >
+                  Confirm
+                </ConfirmButton>
+              )}
 
-              {statusIndex === 1 ? <CancelButton onClick={() => {
-                setStatusIndex(2) 
-              }}>Cancel</CancelButton> : <CancelButton onClick={() => {
-                setStatusIndex(2) 
-              }} disabled>Cancel</CancelButton>}
-              
-          </StyledButtonGroup>
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <CollapsibleTableWrapper>
-            <CollapsibleTable>
-              <CollapsedTitle>
-                <p>Service Name</p>
-                <p>Reference Number</p>
-                <p>Contact Number</p>
-              </CollapsedTitle>
-              <div>
-                <p>Chinese Medicine Consultation </p>
-                <p>R088034029385</p>
-                <p>0414123456</p>
-              </div>
-              <CollapsedTitle>
-                <p>Time of Order</p>
-                <p>Time of Booking</p>
-                <p style={{color: '#8E8E8E'}}>Note</p>
-              </CollapsedTitle>
-              <div>
-                <p>15 Jun 2022  18:00:54</p>
-                <p>18 Jun 2022  13:00</p>
-                <p></p>
-              </div>
-            </CollapsibleTable>
-            <CollapsedNotice>
-              Waiting for comfirmation from store. If no response, order will be cancelled in 16 Jun 2022 18:00
-            </CollapsedNotice>
-          </CollapsibleTableWrapper>
+              {statusIndex === 1 ? (
+                <CancelButton
+                  onClick={() => {
+                    setStatusIndex(2);
+                  }}
+                >
+                  Cancel
+                </CancelButton>
+              ) : (
+                <CancelButton
+                  onClick={() => {
+                    setStatusIndex(2);
+                  }}
+                  disabled
+                >
+                  Cancel
+                </CancelButton>
+              )}
+            </StyledButtonGroup>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <CollapsibleTableWrapper>
+              <CollapsibleTable>
+                <CollapsedTitle>
+                  <p>Service Name</p>
+                  <p>Reference Number</p>
+                  <p>Contact Number</p>
+                </CollapsedTitle>
+                <div>
+                  <p>Chinese Medicine Consultation </p>
+                  <p>R088034029385</p>
+                  <p>0414123456</p>
+                </div>
+                <CollapsedTitle>
+                  <p>Time of Order</p>
+                  <p>Time of Booking</p>
+                  <p style={{ color: "#8E8E8E" }}>Note</p>
+                </CollapsedTitle>
+                <div>
+                  <p>15 Jun 2022 18:00:54</p>
+                  <p>18 Jun 2022 13:00</p>
+                  <p></p>
+                </div>
+              </CollapsibleTable>
+              <CollapsedNotice>
+                Waiting for comfirmation from store. If no response, order will
+                be cancelled in 16 Jun 2022 18:00
+              </CollapsedNotice>
+            </CollapsibleTableWrapper>
           </Collapse>
         </TableRow>
       </StyledTableContainer>
-      </>
-  )
+    </>
+  );
 };
