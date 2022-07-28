e3bcbdf714ec7dc9bbf4bb0992d212813a0a33da
Chris Wang <chris.wang@tcam.co>
2022-07-28 15:28:07 +1000

    Update booking page

--- front_end/src/pages/BookingPage/BookingPage.jsx
+++ front_end/src/pages/BookingPage/BookingPage.jsx
@@ -6,15 +6,18 @@ import BookingPanel from './components/BookingPanel/BookingPanel';
 import { useParams } from 'react-router-dom';
 import { useGetStoreQuery } from '../../store/api/storeApi';
 import ServiceList from './components/ServiceList';
+import { Box } from '@mui/material';
 
 const PageContainer = styled.div`
-  width: 1233px;
+  width: 1240px;
   margin: 100px auto;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
 `;
 
 const StoreInfoWrapper = styled.div`
-  width: 95%;
+  width: 96%;
   margin: -25px auto;
   display: flex;
   justify-content: space-between;
@@ -32,12 +35,23 @@ const BookingPage = () => {
       {isLoading && <p>Loading...</p>}
       {isSuccess && (
         <PageContainer>
-          <Banner />
-          <StoreInfoWrapper>
-            <StoreInfo store={data} id={_id} />
-            <BookingPanel id={_id} />
-          </StoreInfoWrapper>
-          <ServiceList id={_id} />
+          <Box
+            sx={{
+              width: '1180px',
+              border: '1px solid rgba(255, 255, 255, 0.15)',
+              backdropFilter: 'blur(20px)',
+              borderRadius: '10px',
+              background:
+                'linear-gradient(250.42deg, rgba(255, 255, 255, 0.32) 0%, rgba(255, 255, 255, 0.08) 101.65%)',
+            }}
+          >
+            <Banner />
+            <StoreInfoWrapper>
+              <StoreInfo store={data} id={_id} />
+              <BookingPanel id={_id} />
+            </StoreInfoWrapper>
+            <ServiceList id={_id} />
+          </Box>
         </PageContainer>
       )}
     </>
