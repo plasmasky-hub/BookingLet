7cb629849b771c91e78199fb6c2d6c1c83a0cad3
RedRe4per <s3582474@gmail.com>
2022-08-10 19:17:45 +1000

    Back-end order retrieve & pagination

--- back_end/controllers/order.controller.js
+++ back_end/controllers/order.controller.js
@@ -5,7 +5,8 @@ const ServiceInfo = require('../models/serviceInfo');
 const {
   checkTimeIntervalAndBook,
   bookingWithdraw,
-  checkDateFormat
+  checkDateFormat,
+  getWeekMonday
 } = require('./calendar.controller');
 const { any } = require('joi');
 
@@ -90,6 +91,7 @@ async function addOrder(req, res) {
         optionInfo,
         bookingTime,
       });
+      createTimestamp(newOrder);
       await newOrder.save();
 
       const user = await User.findById(userId).exec();
@@ -116,6 +118,8 @@ async function updateOrderByID(req, res) {
   const { id } = req.params;
   const { peopleNumber, orderTime, tel, optionInfo } = req.body;
   const bookingTime = new Date();
+  createTimestampInUpdate(orderTime);
+
   const order = await Order.findByIdAndUpdate(
     id,
     { peopleNumber, orderTime, tel, optionInfo, bookingTime },
@@ -126,6 +130,8 @@ async function updateOrderByID(req, res) {
   }
   return res.status(200).json(order);
 }
+
+
 // Store confirm order ->change booking status from false to true
 async function confirmOrder(req, res) {
   const { id } = req.params;
@@ -161,17 +167,8 @@ async function cancelOrder(req, res) {
   await order.save();
   res.send(order);
 }
-//delete
-// async function deleteOrderByID(req,res){
-//     const { id } = req.params;
-//     const order = await Order.findByIdAndDelete(id).exec();
-//     if (!order) {
-//       return res.status(404).json({ error: 'Order not found' });
-//     }
-//     // return res.sendStatus(200).json("Order has been deleted!");
-//     return res.sendStatus(200)
-// };
-//get one
+
+
 async function getOrderByID(req, res) {
   const { id } = req.params;
   const order = await Order.findById(id).exec();
@@ -183,18 +180,81 @@ async function getOrderByID(req, res) {
 
 
 async function getAllOrders(req, res) {
-  const { userId, storeId, showAll = false } = req.query;
+  const { userId, storeId, serviceInfoId, periodLimiter, page, onlyShowUnconfirmedBooking = false } = req.query;
+  let periodLimiterEnum = ['coming', 'allPrev', 'yearPrev', 'monthPrev', 'weekPrev'];
+  if (periodLimiter !== undefined && periodLimiterEnum.indexOf(periodLimiter) < 0) {
+    return res.status(400).json('Incorrect period limiter. It must be one of [coming, allPrev, yearPrev, monthPrev, weekPrev].')
+  };
+
+  const reg = /^\d+$/;
+  if (page !== undefined && (!reg.test(page) || parseInt(page) < 1)) {
+    return res.status(400).json('Incorrect page format. It must be a number greater than 1.');
+  }
+
   let findQuery = {};
   if (storeId !== undefined) { findQuery.storeId = storeId };
   if (userId !== undefined) { findQuery.userId = userId };
-  let qty = showAll ? 99999 : 999;
+  if (serviceInfoId !== undefined) { findQuery.serviceInfoId = serviceInfoId };
+  if (onlyShowUnconfirmedBooking) { findQuery.bookingStatus = false };
+  let qty = page === undefined ? 99999 : 6;
+  let pagination = page === undefined ? 0 : (parseInt(page)-1) * 6;
 
-  
-  const orders = await Order.find(findQuery).sort({ bookingTime: -1 }).populate('storeId', 'name location tel').populate('userId', 'name tel email').populate('serviceInfoId', 'name description').limit(qty).exec();
-  if (!orders) {
-    return res.status(400).json({ error: 'Order not found' });
-  }
-  return res.status(200).json(orders);
+  const currentTime = new Date();
+  const currentDateStart = new Date(`${currentTime.getFullYear()}-${(currentTime.getMonth() + 1) < 10 ? '0' + (currentTime.getMonth() + 1) : (currentTime.getMonth() + 1)}-${(currentTime.getDate()) < 10 ? '0' + (currentTime.getDate()) : (currentTime.getDate())} 00:00:00`);
+  const currentYearStart = new Date(`${currentTime.getFullYear()}-01-01 00:00:00`);
+  const currentMonthStart = new Date(`${currentTime.getFullYear()}-${(currentTime.getMonth() + 1) < 10 ? '0' + (currentTime.getMonth() + 1) : (currentTime.getMonth() + 1)}-01 00:00:00`);
+  const currentWeekStart = getWeekMonday(currentDateStart);
+
+  if (periodLimiter === 'coming') { findQuery['orderTime.timestamp'] = { $gte: currentTime } };
+  if (periodLimiter === 'allPrev') { findQuery['orderTime.timestamp'] = { $lt: currentTime } };
+  if (periodLimiter === 'yearPrev') { findQuery['orderTime.timestamp'] = { $lt: currentTime, $gte: currentYearStart } };
+  if (periodLimiter === 'monthPrev') { findQuery['orderTime.timestamp'] = { $lt: currentTime, $gte: currentMonthStart } };
+  if (periodLimiter === 'weekPrev') { findQuery['orderTime.timestamp'] = { $lt: currentTime, $gte: currentWeekStart } };
+
+  const matchedOrders = await Order.find(findQuery).exec();
+  const pageQty = Math.ceil(matchedOrders.length/6);
+
+  const orders = await Order.find(findQuery).sort({ bookingTime: -1 })
+    .populate('storeId', 'name location tel').populate('userId', 'name tel email').populate('serviceInfoId', 'name description').limit(qty).skip(pagination).exec();
+  if (!orders) { return res.status(400).json({ error: 'Order not found' }); }
+  return res.status(200).json({pageQty, orders});
+}
+
+
+function createTimestamp(order) {
+  let orderYear = order.orderTime.date.getFullYear() + '';
+  let orderMonth = order.orderTime.date.getMonth() < 9 ? ('0' + (order.orderTime.date.getMonth() + 1)) : ((order.orderTime.date.getMonth() + 1) + '');
+  let orderDate = order.orderTime.date.getDate() < 10 ? ('0' + order.orderTime.date.getDate()) : (order.orderTime.date.getDate() + '')
+
+  let startTime = order.orderTime.startTime.length < 2 ? ('000' + order.orderTime.startTime)
+    : order.orderTime.startTime.length < 3 ? ('00' + order.orderTime.startTime)
+      : order.orderTime.startTime.length < 4 ? ('0' + order.orderTime.startTime)
+        : order.orderTime.startTime;
+
+  let orderHour = startTime.substring(0, 2);
+  let orderMinute = startTime.substring(2, 4);
+  let orderSecond = '00';
+
+  order.orderTime.timestamp = new Date(`${orderYear}-${orderMonth}-${orderDate} ${orderHour}:${orderMinute}:${orderSecond}`);
+}
+
+
+function createTimestampInUpdate(orderTime) {
+  let orderYear = orderTime.date.getFullYear() + '';
+  let orderMonth = orderTime.date.getMonth() < 9 ? ('0' + (orderTime.date.getMonth() + 1)) : ((orderTime.date.getMonth() + 1) + '');
+  let orderDate = orderTime.date.getDate() < 10 ? ('0' + orderTime.date.getDate()) : (orderTime.date.getDate() + '')
+
+  let startTime = orderTime.startTime.length < 2 ? ('000' + orderTime.startTime)
+    : orderTime.startTime.length < 3 ? ('00' + orderTime.startTime)
+      : orderTime.startTime.length < 4 ? ('0' + orderTime.startTime)
+        : orderTime.startTime;
+
+  let orderHour = startTime.substring(0, 2);
+  let orderMinute = startTime.substring(2, 4);
+  let orderSecond = '00';
+
+  orderTime.timestamp = new Date(`${orderYear}-${orderMonth}-${orderDate} ${orderHour}:${orderMinute}:${orderSecond}`);
+  return timestamp;
 }
 
 
